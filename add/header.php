ff0ff0336854a2554b338f136b1efd1e96460eeb
Denor74 <den74@inbox.ru>
2023-07-23 15:48:36 +0500

    css

---
+++ add/header.php
@@ -0,0 +1,9 @@
+<header class="bg-dark bg-gradient text-light">
+        <h2 class="h2-nav">Итоговое задание 24.5.1 (HW-04) SkillFactory</h2>
+        <nav class="nav">
+            <a href="Home">Главная</a>
+            <a href="Sl">О SkillFactory</a>
+            <a href="Contact">Мои контакты</a>
+        </nav>
+
+</header>
--- index.php
+++ index.php
@@ -36,7 +36,6 @@ if (!empty($_FILES)) {
     if (empty($errors)) {
         $messages[] = 'Файлы были загружены';
     }
-
 }
 
 // Если файл был удален
@@ -49,7 +48,7 @@ if (!empty($_POST['name'])) {
     unlink($filePath);
 
     // Удаляем файл комментариев, если он существует
-    if(file_exists($commentPath)) {
+    if (file_exists($commentPath)) {
         unlink($commentPath);
     }
 
@@ -65,90 +64,90 @@ $files = array_filter($files, function ($file) {
 ?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Галерея изображений | Список файлов</title>
 </head>
+
 <body>
-<div class="container pt-4">
-    <h1 class="mb-4"><a href="<?php echo URL; ?>">Галерея изображений</a></h1>
+<?php require_once 'add/header.php'; ?>
+    <main>
+        <div class="container pt-4">
+            <h1 class="mb-4"><a href="<?php echo URL; ?>">Галерея изображений</a></h1>
 
-    <!-- Вывод сообщений об успехе/ошибке -->
-    <?php foreach ($errors as $error): ?>
-        <div class="alert alert-danger"><?php echo $error; ?></div>
-    <?php endforeach; ?>
+            <!-- Вывод сообщений об успехе/ошибке -->
+            <?php foreach ($errors as $error) : ?>
+                <div class="alert alert-danger"><?php echo $error; ?></div>
+            <?php endforeach; ?>
 
-    <?php foreach ($messages as $message): ?>
-        <div class="alert alert-success"><?php echo $message; ?></div>
-    <?php endforeach; ?>
+            <?php foreach ($messages as $message) : ?>
+                <div class="alert alert-success"><?php echo $message; ?></div>
+            <?php endforeach; ?>
 
-    <h2>Список файлов</h2>
+            <h2>Список файлов</h2>
 
-    <!-- Вывод изображений -->
-    <div class="mb-4">
-        <?php if (!empty($files)): ?>
-            <div class="row">
-                <?php foreach ($files as $file): ?>
+            <!-- Вывод изображений -->
+            <div class="mb-4">
+                <?php if (!empty($files)) : ?>
+                    <div class="row">
+                        <?php foreach ($files as $file) : ?>
 
-                    <div class="col-12 col-sm-3 mb-4">
-                        <form method="post">
-                            <input type="hidden" name="name" value="<?php echo $file; ?>">
-                            <button type="submit" class="close" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </form>
-                        <a href="<?php echo URL . '/file.php?name=' . $file; ?>" title="Просмотр полного изображения">
-                            <img src="<?php echo URL . '/' . UPLOAD_DIR . '/' . $file ?>" class="img-thumbnail"
-                                 alt="<?php echo $file; ?>">
-                        </a>
-                    </div>
- 
-                <?php endforeach; ?>
-            </div><!-- /.row -->
-        <?php else: ?>
-            <div class="alert alert-secondary">Нет загруженных изображений</div>
-        <?php endif; ?>
-    </div>
+                            <div class="col-12 col-sm-3 mb-4">
+                                <form method="post">
+                                    <input type="hidden" name="name" value="<?php echo $file; ?>">
+                                    <button type="submit" class="close" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </form>
+                                <a href="<?php echo URL . '/file.php?name=' . $file; ?>" title="Просмотр полного изображения">
+                                    <img src="<?php echo URL . '/' . UPLOAD_DIR . '/' . $file ?>" class="img-thumbnail" alt="<?php echo $file; ?>">
+                                </a>
+                            </div>
+
+                        <?php endforeach; ?>
+                    </div><!-- /.row -->
+                <?php else : ?>
+                    <div class="alert alert-secondary">Нет загруженных изображений</div>
+                <?php endif; ?>
+            </div>
 
 
-    <!-- Форма загрузки файлов -->
-    <form method="post" enctype="multipart/form-data">
-        <div class="custom-file">
-            <input type="file" class="custom-file-input" name="files[]" id="customFile" multiple required>
-            <label class="custom-file-label" for="customFile" data-browse="Выбрать">Выберите файлы</label>
-            <small class="form-text text-muted">
-                Максимальный размер файла: <?php echo UPLOAD_MAX_SIZE / 1000000; ?>Мб.
-                Допустимые форматы: <?php echo implode(', ', ALLOWED_TYPES) ?>.
-            </small>
-        </div>
-        <hr>
-        <button type="submit" class="btn btn-primary">Загрузить</button>
-    </form>
-</div><!-- /.container -->
-
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input@1.3.4/dist/bs-custom-file-input.min.js"></script>
-<script>
-    $(() => {
-        bsCustomFileInput.init();
-    });
-</script>
+            <!-- Форма загрузки файлов -->
+            <form method="post" enctype="multipart/form-data">
+                <div class="custom-file">
+                    <input type="file" class="custom-file-input" name="files[]" id="customFile" multiple required>
+                    <label class="custom-file-label" for="customFile" data-browse="Выбрать">Выберите файлы</label>
+                    <small class="form-text text-muted">
+                        Максимальный размер файла: <?php echo UPLOAD_MAX_SIZE / 1000000; ?>Мб.
+                        Допустимые форматы: <?php echo implode(', ', ALLOWED_TYPES) ?>.
+                    </small>
+                </div>
+                <hr>
+                <button type="submit" class="btn btn-primary">Загрузить</button>
+            </form>
+        </div><!-- /.container -->
+    </main>
+    <footer class="fixed-bottom bg-dark bg-gradient text-light">
+        <p>&copy; Денис Рыков </p>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input@1.3.4/dist/bs-custom-file-input.min.js"></script>
+    <script>
+        $(() => {
+            bsCustomFileInput.init();
+        });
+    </script>
 </body>
+
 </html>
